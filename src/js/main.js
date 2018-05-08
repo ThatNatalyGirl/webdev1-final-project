e2e518f3f70b7751861d5c33c576e8533861132d
Nataly <ncgarzon88@gmail.com>
2018-05-07 21:23:16 -0400

    edited the hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,27 @@
-var dropdown = document.querySelector("header nav ul");
+var firstNav = document.querySelector("header nav .first-nav");
+var secondNav = document.querySelector("header nav .second-nav");
 var burger = document.querySelector(".hamburger");
-var x = document.querySelector("nav ul button")
+var x = document.querySelector("nav .first-nav button")
 
-dropdown.style.display ="none";
+firstNav.style.display ="none";
 
 burger.addEventListener("click", function(){
-	burger.style.display = "none"
-	if( dropdown.style.display === "none" ) {
-		dropdown.style.display = "block";
+	burger.style.display = "none";
+	if( firstNav.style.display === "none" ) {
+		firstNav.style.display = "block";
 	}	
 });
 
 x.addEventListener("click", function(){
-	dropdown.style.display = "none"
-	burger.style.display = "block"
+	firstNav.style.display = "none";
+	burger.style.display = "block";
 });
+
+var nav = function () {
+	if(secondNav.style.display === "flex"){
+		burger.style.display ="none";
+	}
+//making it so if second nav is showing first nav wont show and vice versa
+}
+
+nav();
